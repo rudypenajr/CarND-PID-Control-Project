aca3e1b3c7c7d0fc8a71adf2cbfdc914420beaac
Rudy Pena <penar@us.ibm.com>
2017-07-11 20:23:59 -0500

    feat(pid): watched udacity youtube video on pid project, helped with good basis for starting

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,6 +1,6 @@
 #include "PID.h"
 
-using namespace std;
+// using namespace std;
 
 /*
 * TODO: Complete the PID class.
@@ -11,11 +11,33 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  /*
+   * Errors
+  */
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+
+  /*
+   * Coefficients
+  */
+  this->Kp = Kp;
+  this->Ki = Kd;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  // d_error is how much we have changed since our last crosstrack error
+  d_error = cte - p_error;
+
+  // i_error is sum of our cross track errors
+  i_error += cte;
+
+  // p_error is basically our cross track error
+  p_error = cte;
 }
 
 double PID::TotalError() {
+  // steer = -tau_p * cte - tau_d * diff_cte - tau_i * int_cte
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
-
